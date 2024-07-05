9bc5405cd79c7965713033d6eb6dbe4d65734f4f
gabrielsouzaleite <gabriel.s.leite05@gmail.com>
2024-07-05 16:25:56 -0300

    Primeiro commit do projeto :fire:

---
+++ Petfolio.Application/UseCases/Pet/Register/RegisterPetUseCase.cs
@@ -0,0 +1,17 @@
+﻿using Petfolio.Communication.Requests;
+using Petfolio.Communication.Responses;
+
+namespace Petfolio.Application.UseCases.Pet.Register;
+
+public class RegisterPetUseCase
+{
+    public ResponseRegisterPetJson Execute(RequestRegisterPetJson request)
+    {
+        return new ResponseRegisterPetJson
+        {
+            Id = 1,
+            Name = request.Name,
+        };
+
+    }
+}
